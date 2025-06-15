1defb467e87b727abe2758c4a75caddf0efed6b1
Emmanuel Alves <alvesmendes860@gmail.com>
2025-06-15 18:27:22 -0300

    Subindo criação do projeto #2

---
+++ src/main/kotlin/br/com/financeControl/FinanceControl/FinanceControlApplication.kt
@@ -0,0 +1,11 @@
+package br.com.financeControl.FinanceControl
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FinanceControlApplication
+
+fun main(args: Array<String>) {
+	runApplication<FinanceControlApplication>(*args)
+}
